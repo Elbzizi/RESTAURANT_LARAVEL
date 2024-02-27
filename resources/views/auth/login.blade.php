30e0aa4c3909170935b1defb250c62023e0d3974
omar <omar@tzst.com>
2024-02-27 22:11:44 +0100

    fin page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">{{ __('se connecter') }}</div>
+                <div class="card-header text-light bg-success text-center">{{ __('se connecter') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -52,7 +52,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-success">
                                     {{ __('se connecter') }}
                                 </button>
 
