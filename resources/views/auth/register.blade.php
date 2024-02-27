79872cdc00ed453c89ef637ff03f020fe76c7e48
omar <omar@tzst.com>
2024-02-27 22:20:36 +0100

    fin de page de  registre

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">{{ __('Registre') }}</div>
+                <div class="card-header bg-primary text-center text-light">{{ __('Registre') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -62,7 +62,7 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-4 ">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Registre') }}
                                 </button>
