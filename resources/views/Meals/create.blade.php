32d619c9e3942096aaebb362165e9824e771be05
omar <omar@tzst.com>
2024-03-01 20:54:52 +0100

    parte de Meals

--- resources/views/Meals/create.blade.php
+++ resources/views/Meals/create.blade.php
@@ -5,13 +5,36 @@
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
 
 <div class="container" >
-     <div class="row justify-content-center">
-        
+     <div class="row justify-content-center" dir="rtl">
+     <div class="col-md-4">
+             <div class="card">
+                 <div class="card-header bg-danger text-light text-center">Menu</div>
+                 <div class="card-body text-right">
+                     <ul class="list-group">
+                         <a href="{{ route('category.index') }}" class="list-group-item list-group-item-action">Ajouter catégorie</a>
+                         <a href="" class="list-group-item list-group-item-action">Rebas</a>
 
+                         <a href="" class="list-group-item list-group-item-action" >Commande client</a>
+
+                     </ul>
+                 </div>
+             </div>
+             @if (count($errors) > 0)
+                 <div class="card mt-5">
+                     <div class="card-body">
+                         <div class="alert alert-danger">
+                             @foreach ($errors->all() as $error)
+                                 <p> {{ $error }}
+                                 <p>
+                             @endforeach
+                         </div>
+                     </div>
+                 </div>
+             @endif
+         </div>
          <div class="col-md-8">
             <div class="card">
                 <div class="card-header bg-danger text-center text-light">Robas</div>
-
                   <form action="" method="post" enctype="multipart/form-data">
                     @csrf
                     <div class="card-body text-right">
@@ -23,34 +46,20 @@
                             <label for="description">Description de Ropas</label>
                             <textarea class="form-control" name="description"></textarea>
                         </div>
-
                         <div class="row">
                             <div class="col">
                                 <label>Prix ($)</label>
                                 <input type="number" name="price" class="form-control" placeholder="Prix de Robas">
                             </div>
-
                         </div>
-
-
-
-
                         <div class="form-group">
                             <h5>Choise Une Catégorie<span class="text-danger">*</span></h5>
                             <div class="controls">
 
                                 <select name="category" class="form-control" required="">
                                     <option value="" selected="" disabled="">Choise Une Catégorie</option>
-                                   
                                 </select>
-
-
-
-
-
-
                                 <br>
-
                                 <div class="form-group">
                                     <label>Image de Ropas</label>
                                     <input type="file" name="image" class="form-control" id="image">
@@ -78,7 +87,7 @@
 
 
 
-     
+
      <script type="text/javascript">
         $(document).ready(function(){
             $('#image').change(function(e){
