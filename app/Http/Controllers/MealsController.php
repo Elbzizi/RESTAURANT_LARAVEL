60fb9de16e3c5c59b45b2f34c0834983fb3b3c93
omar <omar@tzst.com>
2024-03-01 20:46:00 +0100

    parté de maels

--- app/Http/Controllers/MealsController.php
+++ app/Http/Controllers/MealsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MaesController extends Controller
+class MealsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -19,7 +19,7 @@ class MaesController extends Controller
      */
     public function create()
     {
-        //
+        return view('Meals.create');
     }
 
     /**
