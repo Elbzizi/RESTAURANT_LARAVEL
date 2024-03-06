fd9513e60bc989147027b375199af7bfbfdb8563
omar <omar@tzst.com>
2024-03-06 21:24:37 +0100

    update meals

--- app/Http/Requests/MealRequest.php
+++ app/Http/Requests/MealRequest.php
@@ -24,8 +24,8 @@ class MealRequest extends FormRequest
     {
 
         return [
-            "name" => ["required", Rule::unique("meals")->ignore($this->meal) ],
-            // 'name' => 'required|unique:meals,name'.$this->id, 
+            // "name" => ["required", Rule::unique("meals")->ignore($this->meal) ],
+            'name' => 'required|unique:meals,name', 
             "description" => "required|string|max:250",
             "price" => "numeric|required",
             "image" => "mimes:png,jpeg,jpg|max:8000",
