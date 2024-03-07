18b8bc75f5ab0828d044d110753e3a87175ff6f1
omar <omar@tzst.com>
2024-03-07 19:18:04 +0100

    les cadres des produits

--- resources/views/components/meal.blade.php
+++ resources/views/components/meal.blade.php
@@ -8,19 +8,3 @@
         <a href="#" class="btn btn-primary">Commandé</a>
     </div>
 </div>
-<div class="card" style="width: 18rem;">
-    <img src="{{ $image }}" class="card-img-top" alt="{{ $image }}">
-    <div class="card-body">
-        <h5 class="card-title">{{ $name }}</h5>
-        <p class="card-text">{{ $description }}</p>
-        <a href="#" class="btn btn-primary">Commandé</a>
-    </div>
-</div>
-<div class="card" style="width: 18rem;">
-    <img src="{{ $image }}" class="card-img-top" alt="{{ $image }}">
-    <div class="card-body">
-        <h5 class="card-title">{{ $name }}</h5>
-        <p class="card-text">{{ $description }}</p>
-        <a href="#" class="btn btn-primary">Commandé</a>
-    </div>
-</div>
